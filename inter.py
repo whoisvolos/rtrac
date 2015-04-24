3d24d3843e5276c13d92662658adaa7f6a67d59b
Sergey Golubev <whoisvolos@gmail.com>
2015-04-24 18:16:27 +0300

    Indentations

--- inter.py
+++ inter.py
@@ -3,24 +3,24 @@ import numpy.linalg as la
 from collections import namedtuple
 
 class Ray3:
-	def __init__(self, p0, u):
-		self.p0 = np.array(p0)
-		npu = np.array(u)
-		self.u = npu / la.norm(npu)
+    def __init__(self, p0, u):
+        self.p0 = np.array(p0)
+        npu = np.array(u)
+        self.u = npu / la.norm(npu)
 
-	def intr_plane(self, plane):
-		denom = np.dot(plane.n, self.u)
-		if (denom >= 0):
-			return None
-		else:
-			s = np.dot(plane.n, plane.v0 - ray.p0) / denom
-			return self.p0 + s * self.u
+    def intr_plane(self, plane):
+        denom = np.dot(plane.n, self.u)
+        if (denom >= 0):
+            return None
+        else:
+            s = np.dot(plane.n, plane.v0 - ray.p0) / denom
+            return self.p0 + s * self.u
 
 class Plane3:
-	def __init__(self, v0, n):
-		self.v0 = np.array(v0)
-		npn = np.array(n)
-		self.n = npn / la.norm(npn)
+    def __init__(self, v0, n):
+        self.v0 = np.array(v0)
+        npn = np.array(n)
+        self.n = npn / la.norm(npn)
 
 # (p0, u)
 ray = Ray3(p0 = [1, 0, 2], u = [2, 0, -1])
@@ -29,4 +29,4 @@ ray = Ray3(p0 = [1, 0, 2], u = [2, 0, -1])
 plane = Plane3(v0 = [1, 0, 0.5], n = [0, 0, 0.5])
 
 for i in range(0, 100000):
-	ray.intr_plane(plane)
+    ray.intr_plane(plane)
